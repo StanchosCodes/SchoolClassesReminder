07a341aa26772fb79f13a61f6a16959f904970d4
Stanislav Stamatov <Stanislav.Stamatov45@gmail.com>
2025-02-22 18:19:20 +0200

    Initial commit of the project

---
+++ SchoolClassesReminder/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows.Media;
+using Win = System.Windows;
+using WinForms = System.Windows.Forms;
+using Drawing = System.Drawing;
+
+namespace SchoolClassesReminder
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Win.Application
+    {
+        private readonly WinForms.NotifyIcon notifyIcon;
+
+        public App()
+        {
+            this.notifyIcon = new NotifyIcon();
+        }
+
+        protected override void OnStartup(Win.StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            this.notifyIcon.Icon = new System.Drawing.Icon("Resources/instagram_icon.ico");
+            this.notifyIcon.Text = "Classes Reminder";
+            this.notifyIcon.Click += NotifyIcon_Click;
+
+            this.notifyIcon.ContextMenuStrip = new WinForms.ContextMenuStrip();
+            this.notifyIcon.ContextMenuStrip.Items.Add("Test", Drawing.Image.FromFile("Resources/instagram_icon.ico"), OnTestClicked);
+
+            this.notifyIcon.Visible = true;
+
+            this.notifyIcon.ShowBalloonTip(3000, "Application started", "The School classes reminder is now in your apps tray!", ToolTipIcon.Info);
+
+            base.OnStartup(e);
+        }
+
+        private void OnTestClicked(object? sender, EventArgs e)
+        {
+            Win.MessageBox.Show("It works!");
+        }
+
+        private void NotifyIcon_Click(object? sender, EventArgs e)
+        {
+            MainWindow.WindowState = Win.WindowState.Normal;
+            MainWindow.Activate();
+        }
+        protected override void OnExit(Win.ExitEventArgs e)
+        {
+            this.notifyIcon.Dispose();
+
+            base.OnExit(e);
+        }
+    }
+
+}
---
+++ SchoolClassesReminder/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows.Threading;
+using Win = System.Windows;
+
+namespace SchoolClassesReminder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Win.Window
+    {
+        private int numberOfClasses;
+        private int durationOfClasses;
+        private int durationOfRecesses;
+        private int durationOfBigRecess;
+        private TimeSpan timeLeft;
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.numberOfClasses = 0;
+            this.durationOfClasses = 0;
+            this.durationOfRecesses = 0;
+            this.durationOfBigRecess = 0;
+            this.timer = new DispatcherTimer();
+        }
+
+        private void ButtonAddDetails_Click(object sender, Win.RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNumberOfClasses.Text) && !string.IsNullOrEmpty(txtDurationOfClasses.Text) && !string.IsNullOrEmpty(txtDurationOfRecesses.Text))
+            {
+                this.numberOfClasses = int.Parse(txtNumberOfClasses.Text);
+                this.durationOfClasses = int.Parse(txtDurationOfClasses.Text);
+                this.durationOfRecesses = int.Parse(txtDurationOfRecesses.Text);
+                this.durationOfBigRecess = int.Parse(txtDurationOfBigRecess.Text);
+
+                lblNumberOfClasses.Content = this.numberOfClasses;
+                lblDurationOfClasses.Content = this.durationOfClasses + " minutes";
+                lblDurationOfRecesses.Content = this.durationOfRecesses + " minutes";
+                lblDurationOfBigRecess.Content = this.durationOfBigRecess + " minutes";
+
+                txtNumberOfClasses.Clear();
+                txtDurationOfClasses.Clear();
+                txtDurationOfRecesses.Clear();
+                txtDurationOfBigRecess.Clear();
+
+                long durationOfClassesInTicks = this.durationOfClasses * TimeSpan.TicksPerMinute;
+
+                this.timeLeft = new TimeSpan(durationOfClassesInTicks);
+
+                if (this.timer.IsEnabled)
+                {
+                    this.timer.Stop();
+                    this.timer = new DispatcherTimer();
+                }
+
+                this.timer.Interval = TimeSpan.FromSeconds(1);
+                this.timer.Tick += Timer_Tick;
+                this.timer.Start();
+
+                Win.MessageBox.Show("Details added!");
+            }
+            else
+            {
+                Win.MessageBox.Show("Not all fields are filled correctly!");
+            }
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (this.timeLeft.Hours == 0 && this.timeLeft.Minutes == 0 && this.timeLeft.Seconds == 0)
+            {
+                this.timer.Stop();
+                Win.MessageBox.Show("The class has ended. Your recess starts now!");
+            }
+            else
+            {
+                this.timeLeft = this.timeLeft.Subtract(TimeSpan.FromSeconds(1));
+            }
+
+            lblTimer.Content = this.timeLeft;
+        }
+    }
+}
